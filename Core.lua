dcbcea624195afa6ebfaa0934d36ec2551ac2d82
Per Riekkola <per@kumpan.se>
2024-05-14 11:55:03 +0000

    Edits for xp bars

--- Core.lua
+++ Core.lua
@@ -1,31 +1,42 @@
 local Perskan = CreateFrame("Frame")
+defaultBottomMargin = 8
+oneBarMargin = 20
+twoBarMargin = 40
 
 local function Perskan_OnLoad()
-    -- Check if there is a secondary status tracking bar
-    local bottomMargin = 20
-
-    if SecondaryStatusTrackingBarContainer:IsVisible() then
-        bottomMargin = 40
-    end
+    local LibEditModeOverride = LibStub("LibEditModeOverride-1.0")
 
     -- Move actionbars and override edit mode
-    local LibEditModeOverride = LibStub("LibEditModeOverride-1.0")
-    LibEditModeOverride:LoadLayouts()
-    LibEditModeOverride:ReanchorFrame(MainMenuBar, "BOTTOM", UIParent, "BOTTOM", 0, bottomMargin)
-    LibEditModeOverride:ReanchorFrame(MultiBarBottomLeft, "BOTTOM", MainMenuBar, "TOP", 0, 4)
-    LibEditModeOverride:ReanchorFrame(MultiBarBottomRight, "BOTTOM", MultiBarBottomLeft, "TOP", 0, 1)
+    if LibEditModeOverride:IsReady() then
+        LibEditModeOverride:LoadLayouts()
 
-    -- Check if MultiBarBottomRight is activated, then move stance and pet bar
-    local bottomLeftState, bottomRightState, sideRightState, sideRight2State = GetActionBarToggles()
-    if bottomRightState == false then
-        LibEditModeOverride:ReanchorFrame(StanceBar, "BOTTOMLEFT", MultiBarBottomLeftButton1, "TOPLEFT", 0, 3)
-        LibEditModeOverride:ReanchorFrame(PetActionBar, "BOTTOMRIGHT", MultiBarBottomLeftButton12, "TOPRIGHT", -12, 3)
-    else
-        LibEditModeOverride:ReanchorFrame(StanceBar, "BOTTOMLEFT", MultiBarBottomRightButton1, "TOPLEFT", 0, 3)
-        LibEditModeOverride:ReanchorFrame(PetActionBar, "BOTTOMRIGHT", MultiBarBottomRightButton12, "TOPRIGHT", -12, 3)
+        -- Check if there is a status tracking bar
+        local bottomMargin = defaultBottomMargin
+        if MainStatusTrackingBarContainer:IsVisible() then
+            bottomMargin = oneBarMargin
+        end
+        if SecondaryStatusTrackingBarContainer:IsVisible() then
+            bottomMargin = twoBarMargin
+        end
+
+        -- Reanchor actionbars
+        LibEditModeOverride:ReanchorFrame(MainMenuBar, "BOTTOM", UIParent, "BOTTOM", 0, bottomMargin)
+        LibEditModeOverride:ReanchorFrame(MultiBarBottomLeft, "BOTTOM", MainMenuBar, "TOP", 0, 4)
+        LibEditModeOverride:ReanchorFrame(MultiBarBottomRight, "BOTTOM", MultiBarBottomLeft, "TOP", 0, 1)
+
+        -- Check if MultiBarBottomRight is activated, then move stance and pet bar
+        local bottomLeftState, bottomRightState, sideRightState, sideRight2State = GetActionBarToggles()
+        if bottomRightState == false then
+            LibEditModeOverride:ReanchorFrame(StanceBar, "BOTTOMLEFT", MultiBarBottomLeftButton1, "TOPLEFT", 0, 3)
+            LibEditModeOverride:ReanchorFrame(PetActionBar, "BOTTOMRIGHT", MultiBarBottomLeftButton12, "TOPRIGHT", -2, 3)
+        else
+            LibEditModeOverride:ReanchorFrame(StanceBar, "BOTTOMLEFT", MultiBarBottomRightButton1, "TOPLEFT", 0, 3)
+            LibEditModeOverride:ReanchorFrame(PetActionBar, "BOTTOMRIGHT", MultiBarBottomRightButton12, "TOPRIGHT", -2,
+                3)
+        end
+
+        LibEditModeOverride:ApplyChanges()
     end
-
-    LibEditModeOverride:ApplyChanges()
 
     -- Hard position UIParentBottomManagedFrameContainer
     local dummy = function()
@@ -37,8 +48,34 @@ local function Perskan_OnLoad()
 
     -- Scale various UI frames
     EncounterBar:SetScale(0.8)
+
+    -- Hide gryphons
+    MainMenuBar.EndCaps.LeftEndCap:Hide()
+    MainMenuBar.EndCaps.RightEndCap:Hide()
 end
 
 Perskan:RegisterEvent("PLAYER_LOGIN")
-Perskan:RegisterEvent("ACTIONBAR_SHOWGRID")
 Perskan:SetScript("OnEvent", Perskan_OnLoad)
+
+SettingsPanel:HookScript("OnHide", Perskan_OnLoad)
+
+local function MoveMainMenuBar(margin)
+    MainMenuBar:ClearAllPoints()
+    MainMenuBar:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, margin)
+end
+
+MainStatusTrackingBarContainer:HookScript("OnHide", function()
+    MoveMainMenuBar(defaultBottomMargin)
+end)
+
+MainStatusTrackingBarContainer:HookScript("OnShow", function()
+    MoveMainMenuBar(oneBarMargin)
+end)
+
+SecondaryStatusTrackingBarContainer:HookScript("OnHide", function()
+    MoveMainMenuBar(oneBarMargin)
+end)
+
+SecondaryStatusTrackingBarContainer:HookScript("OnShow", function()
+    MoveMainMenuBar(twoBarMargin)
+end)
