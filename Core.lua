7319713fa769faf404cf5b865bf79cad262c4b18
Per Riekkola <per@kumpan.se>
2024-07-03 13:40:23 +0000

    scaling debuffs on nameplates

--- Core.lua
+++ Core.lua
@@ -31,6 +31,14 @@ local function Perskan_OnLoad()
 
     -- Scale various UI frames
     EncounterBar:SetScale(0.8)
+
+    -- Scale nameplate debuffs
+    local function MoveDebuffs(self)
+        if self.BuffFrame then
+            self.BuffFrame:SetScale(1.1)
+        end
+    end
+    hooksecurefunc("CompactUnitFrame_OnLoad", MoveDebuffs)
 end
 
 Perskan:RegisterEvent("PLAYER_ENTERING_WORLD")
