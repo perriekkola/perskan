04ce00556fabf83f6bc73aa5b17a4aa2a778e7d8
Per Riekkola <per@kumpan.se>
2024-07-04 13:28:19 +0000

    Other inset

--- Cvars.lua
+++ Cvars.lua
@@ -9,7 +9,7 @@ CVars:HookScript("OnEvent", function()
     -- Misc
     C_CVar.SetCVar("cameraPivot", 0)
     C_CVar.SetCVar("nameplateOtherBottomInset", 0.1)
-    C_CVar.SetCVar("nameplateOtherTopInset", 0.08)
+    C_CVar.SetCVar("nameplateOtherTopInset", 0.09)
     C_CVar.SetCVar("nameplateOtherBottomInset", -1) --  0.1
     C_CVar.SetCVar("cameraDistanceMaxZoomFactor", 2.3) -- 1.9 
 end);
