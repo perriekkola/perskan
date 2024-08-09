04bf62751764017757a3271a221324ae8752f418
Per Riekkola <per@kumpan.se>
2024-08-09 13:49:09 +0000

    Cleaned up

--- Core.lua
+++ Core.lua
@@ -1,24 +1,27 @@
-local Perskan = CreateFrame("Frame")
+local f = CreateFrame("Frame")
+
 local settingsLoaded = false
 
-local function Perskan_OnLoad(self, event, ...)
-    -- Scale various UI frames
-    EncounterBar:SetScale(0.8)
+function f:OnEvent(event, addOnName)
+    if event == "PLAYER_ENTERING_WORLD" then
+        -- Scale various UI frames
+        EncounterBar:SetScale(0.8)
 
-    -- Set a stealable texture even if you have no purge
-    local function TargetFrame_UpdateAuras(self)
-        for buff in self.auraPools:GetPool("TargetBuffFrameTemplate"):EnumerateActive() do
-            local buffSize = buff.GetHeight(buff)
-            local data = C_UnitAuras.GetAuraDataByAuraInstanceID(buff.unit, buff.auraInstanceID)
-            buff.Stealable:SetShown(data.isStealable or data.dispelName == "Magic")
-            local stealableSize = buffSize + 2
-            buff.Stealable:SetSize(stealableSize, stealableSize)
-            buff.Stealable:SetPoint("CENTER", buff, "CENTER")
+        -- Set a stealable texture even if you have no purge
+        local function TargetFrame_UpdateAuras(self)
+            for buff in self.auraPools:GetPool("TargetBuffFrameTemplate"):EnumerateActive() do
+                local buffSize = buff.GetHeight(buff)
+                local data = C_UnitAuras.GetAuraDataByAuraInstanceID(buff.unit, buff.auraInstanceID)
+                buff.Stealable:SetShown(data.isStealable or data.dispelName == "Magic")
+                local stealableSize = buffSize + 2
+                buff.Stealable:SetSize(stealableSize, stealableSize)
+                buff.Stealable:SetPoint("CENTER", buff, "CENTER")
+            end
         end
-    end
 
-    hooksecurefunc(TargetFrame, "UpdateAuras", TargetFrame_UpdateAuras)
-    hooksecurefunc(FocusFrame, "UpdateAuras", TargetFrame_UpdateAuras)
+        hooksecurefunc(TargetFrame, "UpdateAuras", TargetFrame_UpdateAuras)
+        hooksecurefunc(FocusFrame, "UpdateAuras", TargetFrame_UpdateAuras)
+    end
 
     -- Toggle action bars based on spec
     if event == "SETTINGS_LOADED" then
@@ -45,10 +48,9 @@ local function Perskan_OnLoad(self, event, ...)
             Settings.SetValue("PROXY_SHOW_ACTIONBAR_3", true)
         end
     end
-
 end
 
-Perskan:RegisterEvent("PLAYER_ENTERING_WORLD")
-Perskan:RegisterEvent("SETTINGS_LOADED")
-Perskan:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-Perskan:SetScript("OnEvent", Perskan_OnLoad)
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("SETTINGS_LOADED")
+f:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+f:SetScript("OnEvent", f.OnEvent)
