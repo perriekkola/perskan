8ad643c4b5cd39ed1c746cb8daf929c5e61bed18
Per Riekkola <per@kumpan.se>
2024-08-12 17:33:45 +0000

    Hiding windows based on right ui managed frame container content

--- Core.lua
+++ Core.lua
@@ -1,8 +1,6 @@
 local addonName = ...
 local details = _G.Details
 local details1, details2
-local mainDetailsHeight = 88
-local secondaryDetailsHeight = 140
 
 if details then
     details1 = details:GetInstance(1)
@@ -49,157 +47,55 @@ local function HighlightStealableAuras()
     end
 end
 
-local function HasQuestItemInObjectiveTracker()
-    local numQuestWatches = C_QuestLog.GetNumQuestWatches()
-    if not numQuestWatches then
-        return false
-    end
+local isSettingHeight = false
 
-    for i = 1, numQuestWatches do
-        local questID = C_QuestLog.GetQuestIDForQuestWatchIndex(i)
-        if questID then
-            local link, item, charges, showItemWhenComplete = GetQuestLogSpecialItemInfo(questID)
-            if item then
-                return true
-            end
-        end
-    end
-    return false
-end
-
-local function ReanchorDetailsWindows()
-    if (InCombatLockdown() and HasQuestItemInObjectiveTracker()) or not Perskan.db.profile.reanchorDetailsWindow or
-        not C_AddOns.IsAddOnLoaded("Details") then
+local function SetObjectiveTrackerHeight()
+    if InCombatLockdown() then
         return
     end
 
-    details1.baseframe:SetWidth(254)
-    details2.baseframe:SetWidth(254)
-
-    local anchor, x
-    local highestArenaFrame = nil
-
-    for i = 1, 5 do
-        local frame = _G["ArenaEnemyMatchFrame" .. i]
-        if frame and frame:IsVisible() then
-            highestArenaFrame = frame
-        end
-    end
-
-    if ObjectiveTrackerFrame:IsVisible() then
-        if not QuestObjectiveTracker:IsVisible() then
-            anchor = ObjectiveTrackerFrame.Header
-            x = 2
-        else
-            anchor = ObjectiveTrackerFrame.NineSlice.Center
-            x = 10
-        end
-    elseif DurabilityFrame:IsVisible() then
-        anchor = DurabilityFrame
-        x = 2
-    elseif Boss1TargetFrame:IsVisible() then
-        anchor = BossTargetFrameContainer
-        x = 2
-    elseif VehicleSeatIndicator:IsVisible() then
-        anchor = VehicleSeatIndicator
-        x = 2
-    elseif highestArenaFrame then
-        anchor = highestArenaFrame
-        x = 20
-    else
-        anchor = MinimapCompassTexture
-        x = -1
-    end
-
-    if details1 then
-        details1.baseframe:ClearAllPoints()
-        details1.baseframe:SetPoint("TOPRIGHT", anchor, "BOTTOMRIGHT", x, -50)
-    end
-
-    if details2 then
-        details2.baseframe:ClearAllPoints()
-        details2.baseframe:SetPoint("TOPLEFT", details1.baseframe, "BOTTOMLEFT", 0, -20)
+    if not isSettingHeight then
+        isSettingHeight = true
+        ObjectiveTrackerFrame:SetHeight(340)
+        isSettingHeight = false
     end
 end
 
-local function AdjustDetailsHeight(instance, maxHeight)
-    if not C_AddOns.IsAddOnLoaded("Details") then
-        return
-    end
+hooksecurefunc(ObjectiveTrackerFrame, "SetHeight", function()
+    SetObjectiveTrackerHeight()
+end)
 
-    if not instance then
-        return
-    end
+local framesToCheck = {DurabilityFrame, VehicleSeatIndicator, Boss1TargetFrame}
 
-    local baseHeight = 32
-    local heightPerPlayer = 27
-    local numGroupMembers = IsActiveBattlefieldArena() and 6 or GetNumGroupMembers()
-    numGroupMembers = math.max(numGroupMembers, 1)
-    local newHeight = math.min(baseHeight + (numGroupMembers * heightPerPlayer), maxHeight)
-
-    local pos_table = instance:CreatePositionTable()
-    pos_table.h = newHeight
-    instance:RestorePositionFromPositionTable(pos_table)
-end
-
-local function ResizeAllDetailsWindows()
-    if not C_AddOns.IsAddOnLoaded("Details") then
-        return
-    end
-
-    AdjustDetailsHeight(details1, mainDetailsHeight)
-    AdjustDetailsHeight(details2, secondaryDetailsHeight)
-    ReanchorDetailsWindows()
+for i = 1, 5 do
+    table.insert(framesToCheck, _G["ArenaEnemyMatchFrame" .. i])
 end
 
 local function ToggleDetailsWindows()
-    if not details1 or not details2 then
-        return
+    local shouldHide = false
+
+    for _, frame in ipairs(framesToCheck) do
+        if frame:IsShown() then
+            shouldHide = true
+            break
+        end
     end
 
-    details1:ShowWindow()
-    details2:ShowWindow()
-
-    if not ObjectiveTrackerFrame:IsVisible() then
-        ResizeAllDetailsWindows()
-        local trackerHeight = ObjectiveTrackerFrame.NineSlice.Center:GetHeight()
-        return
+    if shouldHide then
+        details1:HideWindow()
+        details2:HideWindow()
+    else
+        details1:ShowWindow()
+        details2:ShowWindow()
     end
-
-    C_Timer.After(0.1, function()
-        if not ObjectiveTrackerFrame:IsVisible() then
-            return
-        end
-
-        local trackerHeight = ObjectiveTrackerFrame.NineSlice.Center:GetHeight()
-
-        if trackerHeight > 310 then
-            details1:HideWindow()
-            details2:HideWindow()
-        else
-            details1:ShowWindow()
-            details2:ShowWindow()
-            ResizeAllDetailsWindows()
-        end
-    end)
 end
 
-local function HookReanchorDetailsWindows()
-
-    local function HookFrame(frame)
+local function HookToggleDetailsWindows()
+    for _, frame in ipairs(framesToCheck) do
         if frame then
             frame:HookScript("OnShow", ToggleDetailsWindows)
             frame:HookScript("OnHide", ToggleDetailsWindows)
         end
-    end
-
-    hooksecurefunc(QuestObjectiveTracker, "Update", ToggleDetailsWindows)
-    HookFrame(ObjectiveTrackerFrame)
-    HookFrame(VehicleSeatIndicator)
-    HookFrame(Boss1TargetFrame)
-
-    for i = 1, 5 do
-        HookFrame(_G["ArenaEnemyMatchFrame" .. i])
     end
 end
 
@@ -215,10 +111,9 @@ end
 
 function Perskan:OnEnable()
     self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED", AdjustActionBars)
-    self:RegisterEvent("QUEST_LOG_UPDATE", ToggleDetailsWindows)
-    self:RegisterEvent("GROUP_ROSTER_UPDATE", ResizeAllDetailsWindows)
-    self:RegisterEvent("PLAYER_LOGIN", InitializeCVars)
     self:RegisterEvent("SETTINGS_LOADED")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD", SetObjectiveTrackerHeight)
+    self:RegisterEvent("PLAYER_LOGIN", InitializeCVars)
 end
 
 function Perskan:SETTINGS_LOADED()
@@ -228,6 +123,5 @@ function Perskan:SETTINGS_LOADED()
     AdjustActionBars()
     HighlightStealableAuras()
     ScaleUIFrames()
-    HookReanchorDetailsWindows()
-    ToggleDetailsWindows()
+    HookToggleDetailsWindows()
 end
